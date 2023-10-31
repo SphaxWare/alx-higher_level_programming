9c5d6f7f9764354501019912beade1eca4d9ff75
SphaxWare <itssphax123@gmail.com>
2023-10-31 15:16:09 -0700

    test

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
 def print_last_digit(number):
-    if number > 0:
-        last = number % 10
-    else:
-        last = abs(number) % 10
-        last = last + last * 10
-    return last
+    last = abs(number) % 10
+    print(last, end="")
