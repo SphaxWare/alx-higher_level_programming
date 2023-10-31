d5dc59e285a9be861ae289bcc3a202edf8617e93
SphaxWare <itssphax123@gmail.com>
2023-10-31 14:49:48 -0700

    solve 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last = abs(number) % 10
+    if number > 0:
+        last = number % 10
+    else:
+        last = abs(number) % 10
+        last = last + last * 10
     return last
+t = print_last_digit(-98)
+print(t)
