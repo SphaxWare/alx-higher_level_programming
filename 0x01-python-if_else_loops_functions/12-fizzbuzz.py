210a4f4094a3411ff5b94997f5e6b60b783d0501
SphaxWare <itssphax123@gmail.com>
2023-10-31 15:45:12 -0700

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+            continue
+        if i % 3 == 0:
+            print("Fizz", end=" ")
+            continue
+        if i % 5 == 0:
+            print("Buzz", end=" ")
+            continue
+        else:
+            print(i, end=" ")
+    print()
+fizzbuzz()
