ea1e7007c0bbb54ceeba349b8a814a0245a6a2ae
SphaxWare <itssphax123@gmail.com>
2023-11-02 07:41:38 -0700

    infinity 2.0

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 1:
-        print(0)
     result = 0
     for i in range(1, len(sys.argv)):
         result += int(sys.argv[i])
