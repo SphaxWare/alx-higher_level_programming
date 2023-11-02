7f5fe61730295e8ea6e48fafa14cdb820246aea8
SphaxWare <itssphax123@gmail.com>
2023-11-02 06:55:18 -0700

    args 2.0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,8 @@ if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
         print("0 arguments.")
+    if len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv)))
     else:
         print("{} arguments:".format(len(sys.argv)))
     for i in range(1, len(sys.argv)):
