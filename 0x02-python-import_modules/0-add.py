216e582b93aaa73a59727ed165f3172e3da32da8
SphaxWare <itssphax123@gmail.com>
2023-11-02 05:36:49 -0700

    pycodestyle

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import add_0 as add
 if __name__ == "__main__":
-  a = 1
-  b = 2
-  result = add.add(a, b)
-  print("{} + {} = {}".format(a, b, result))
+    a = 1
+    b = 2
+    result = add.add(a, b)
+    print("{} + {} = {}".format(a, b, result))
