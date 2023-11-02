a45c52b386949f8548f157cf5a39cfba2e947c7a
SphaxWare <itssphax123@gmail.com>
2023-11-02 05:35:32 -0700

    add 4.0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import add_0 as add
-a = 1
-b = 2
-result = add.add(a, b)
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+  a = 1
+  b = 2
+  result = add.add(a, b)
+  print("{} + {} = {}".format(a, b, result))
