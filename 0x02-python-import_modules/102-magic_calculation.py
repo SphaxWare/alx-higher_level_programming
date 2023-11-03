9cf099e1f9205143e6aff98f7b1b3874a92e4f77
SphaxWare <itssphax123@gmail.com>
2023-11-02 18:29:42 -0700

    102 test 3.0

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
     if a < b:
-        add = __import__('magic_calculation_102').add
-        sub = __import__('magic_calculation_102').sub
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
