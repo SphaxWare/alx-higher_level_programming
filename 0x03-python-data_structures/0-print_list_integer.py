ba39699cc6dbd7f978964eebae7d109f88cb13d4
SphaxWare <itssphax123@gmail.com>
2023-11-05 08:29:54 -0800

    removed the test

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in range(0, len(my_list)):
-        num = my_list[i]
-        print(f"{num:d}")
-
-
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+    for i in my_list:
+        print("{}".format(i))
