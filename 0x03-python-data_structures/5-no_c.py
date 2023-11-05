8802bd40cb3e8fa3c2b09a5e70811c32cab105af
SphaxWare <itssphax123@gmail.com>
2023-11-05 11:18:41 -0800

    can u C me now

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    newstr = ""
+    for char in my_string:
+        if char == 'c' or char == 'C':
+            continue
+        newstr += char
+    return newstr
