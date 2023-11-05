160490b9c8225bc506070e80c15ff36ba8ee8b1d
SphaxWare <itssphax123@gmail.com>
2023-11-05 09:18:23 -0800

    test j

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    j = 0
     for i in range((len(my_list)-1), -1, -1):
         print("{:d}".format(my_list[i]))
+        my_list[j] = my_list[i]
+        j+=1
+        
