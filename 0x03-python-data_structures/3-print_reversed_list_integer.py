5b71cddb4818aaff6beaf5ba648b7468800a9860
SphaxWare <itssphax123@gmail.com>
2023-11-05 11:02:18 -0800

    3-

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in range((len(my_list)-1), -1, -1):
-        print("{:d}".format(my_list[i]))
+sh: 1: q: not found
+        for i in range((len(my_list)-1), -1, -1):
+            print("{:d}".format(my_list[i]))
