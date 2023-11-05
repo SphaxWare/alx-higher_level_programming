47db808d750231809c3ebb36bbdc5c026919f226
SphaxWare <itssphax123@gmail.com>
2023-11-05 11:13:02 -0800

    copy replace

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,5 +4,6 @@ def new_in_list(my_list, idx, element):
         return my_list
     elif idx > (len(my_list)-1):
         return my_list
-    new_list = my_list
-    return new_list[idx] = element
+    new_list = my_list[:]
+    new_list[idx] = element
+    return new_list
