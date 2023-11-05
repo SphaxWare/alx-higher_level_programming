569eb4afd988ea1983545b8673de0d276db7d1c2
SphaxWare <itssphax123@gmail.com>
2023-11-05 12:21:51 -0800

    matrix 2.0

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        print("")
-        return
+        return None
     for i in matrix:
+        if len(i) == 0:
+            print()
         for j in i:
             if j == i[(len(i) - 1)]:
                 print("{:d}".format(j))
