78fdbbe5ac27a87032b3fe803700307722b1773f
SphaxWare <itssphax123@gmail.com>
2023-11-05 11:08:09 -0800

    replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx > (len(my_list)-1):
+        return my_list
+    new_list = my_list
+    return new_list[idx] = element
