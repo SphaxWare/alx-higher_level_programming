6c12d9bcc7a0824592ebbbfa416a5a964e371e8f
SphaxWare <itssphax123@gmail.com>
2023-11-05 08:55:47 -0800

    2-replace

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,4 +4,5 @@ def replace_in_list(my_list, idx, element):
         return my_list
     elif idx > (len(my_list)-1):
         return my_list
-    return  my_list[idx] = element
+    else:
+        my_list[idx] = element
