b3e63249d50336b4b909049c1c6c2cf4acff5b56
SphaxWare <itssphax123@gmail.com>
2023-11-06 11:04:24 -0800

    max int

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    max = my_list[0]
+    for i in range(1, len(my_list)):
+        max = my_list[i] if max < my_list[i] else max
+    return max
