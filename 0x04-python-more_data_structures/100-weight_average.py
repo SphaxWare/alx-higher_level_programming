840f33bd1e67ac7a1a3f3eebd6894aed753982d5
SphaxWare <itssphax123@gmail.com>
2023-11-08 15:54:32 -0800

    avg

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is None:
+        return 0
+    add = 0
+    div = 0
+    for i in my_list:
+        mul = 1
+        for j in i:
+            mul *= j
+        add += mul
+    for i in my_list:
+        div += i[1]
+    return add/div
