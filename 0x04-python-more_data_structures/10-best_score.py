4ffd207d3934b6091353e061928ac966096a5882
SphaxWare <itssphax123@gmail.com>
2023-11-08 09:02:50 -0800

    fix 1

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if not a_dictionary:
+       return None
+    max_v = max(a_dictionary.values)
     for k , v in a_ictionary.items():
-        if v == max(a_dictionary.values):
+        if v == max_v:
             return key
     return None 
