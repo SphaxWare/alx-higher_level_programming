ec3124c55c0761431f871ffadd870d974c74a0bd
SphaxWare <itssphax123@gmail.com>
2023-11-08 08:01:51 -0800

    fix 1

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,8 +3,8 @@ def only_diff_elements(set_1, set_2):
     uniq_elements = set()
     for i in set_1:
         if i not in set_2:
-            uniq_elements.add(j)
+            uniq_elements.add(i)
     for i in set_2:
         if i not in set_1:
-            uniq_elements.add(j)
+            uniq_elements.add(i)
     return uniq_elements
