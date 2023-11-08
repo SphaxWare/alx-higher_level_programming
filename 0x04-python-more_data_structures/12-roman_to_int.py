07beb51945ef7eb8bb23578ce4ce4c31470d25e9
SphaxWare <itssphax123@gmail.com>
2023-11-08 15:37:11 -0800

    switched to while loop

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string == None or not isinstance(roman_string, str):
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
-    n = 0 
+    n = 0
+    i = 0
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for i in range(len(roman_string)):
+    while i < len(roman_string):
         current = d[roman_string[i]]
-        if  i + 1 < len(roman_string) and d[roman_string[i + 1]] > current:
+        if i + 1 < len(roman_string) and d[roman_string[i + 1]] > current:
             n += d[roman_string[i + 1]] - current
             i += 2
         else:
