cd2f6e20ccbe8398eff837aa8c67cc8775aef63c
SphaxWare <itssphax123@gmail.com>
2023-11-20 07:35:33 -0800

    pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         count = 0
         for i in range(0, x):
-            print(my_list[i], end ='')
+            print(my_list[i], end='')
             count += 1
         print()
         return count
