90dbcef1ca065baa30865c8315eca35ab37a335c
SphaxWare <itssphax123@gmail.com>
2023-11-20 14:11:39 -0800

    fix1

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,5 +3,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
+    except ValueError as e:
+        print(e)
         return False
