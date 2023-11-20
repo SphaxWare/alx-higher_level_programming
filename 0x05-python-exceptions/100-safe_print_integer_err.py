c1dda54ae813b999b543e1d9c2450fde1d53090b
SphaxWare <itssphax123@gmail.com>
2023-11-20 14:20:53 -0800

    fix 3:now print exception to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,8 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as e:
-        sys.stderr.write(str(e) + '\n')
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
         return False
-    except TypeError:
-        pass
