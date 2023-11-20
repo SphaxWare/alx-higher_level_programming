1fef20dbd3d024b43a0921d82f7df80b85c0a222
SphaxWare <itssphax123@gmail.com>
2023-11-20 09:49:25 -0800

    fix 2

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,8 +11,8 @@ def list_division(my_list_1, my_list_2, list_length):
             except (TypeError, ValueError):
                 print("wrong type")
                 result.append(0)
-    except IndexError:
-        print("out of range")
-        result.append(0)
+            except IndexError:
+                print("out of range")
+                result.append(0)
     finally:
         return result
