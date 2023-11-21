be6e41bb6aba74b9501debb444b3315b488eb74a
SphaxWare <itssphax123@gmail.com>
2023-11-21 07:46:52 -0800

    fix 6

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,22 +1,10 @@
-"""Square module."""
-
-
+#!/usr/bin/python3
 class Square:
-    """Defines an empty square."""
-
+    """Defines a square"""
     def __init__(self, size=0):
-        """Constructor.
-
-        Args:
-            size: square size.
-
-        Raises:
-            TypeError: not int.
-            ValueError: size less than 0.
-        """
-
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if (0 > size):
-            raise ValueError("size must be >= 0")
+        """Initialises the data"""
         self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
