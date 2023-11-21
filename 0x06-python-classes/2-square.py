9a4181f6fd21c103276072ef853453507c52e120
SphaxWare <itssphax123@gmail.com>
2023-11-21 07:33:43 -0800

    added comments 2-square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,8 +4,17 @@
 class Square:
     """Defines an empty square."""
     def __init__(self, size=0):
-        self.__size = size
+        """Constructor
+
+        Args:
+            size: square size
+
+        Raises:
+            TypeError: not int
+            ValueError: size less than 0
+        """
         if (type(size) != int):
             raise TypeError("size must be an integer")
         if (0 > size):
             raise ValueError("size must be >= 0")
+        self.__size = size
