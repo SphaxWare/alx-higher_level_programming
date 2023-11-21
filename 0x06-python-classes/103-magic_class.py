cf3aecb43b93a211b50040085eff4e74a3682b96
SphaxWare <itssphax123@gmail.com>
2023-11-21 14:54:35 -0800

    docstring

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
+"""Define a MagicClass bytecode."""
+
+
 import math
 
 
 class MagicClass:
+    """A circle"""
+
     def __init__(self, radius=0):
+        """Initialize MagicClass.
+        Arg:
+            radius (float or int): radius
+        """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
+        """Return area of circle"""
         return self.__radius**2 * math.pi
 
     def circumference(self):
+        """Return The circumference of the circle"""
         return 2 * math.pi * self.__radius
