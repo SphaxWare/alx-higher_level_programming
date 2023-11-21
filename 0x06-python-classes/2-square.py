2ce3c2aeb55166ebf44ba2f3d5328e583d2f236d
SphaxWare <itssphax123@gmail.com>
2023-11-21 07:26:16 -0800

    size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,11 @@
+"""Square module."""
+
+
+class Square:
+    """Defines an empty square."""
+    def __init__(self, size=0):
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        if (0 > size):
+            raise ValueError("size must be >= 0")
+        self.__size = size
