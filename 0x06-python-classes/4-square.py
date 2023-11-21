474a7874c094bbef63777ec9abdeffbd0bfc31a5
SphaxWare <itssphax123@gmail.com>
2023-11-21 08:23:07 -0800

    fixed socstring

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,19 +6,39 @@ class Square:
     """Defines a square."""
 
     def __init__(self, size=0):
-        self.size = size
+        """Constructor.
 
-    @size.setter
-    def size(self, size):
-        self.__size = size
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if (0 > size):
-            raise ValueError("size must be >= 0")
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        self.size = size
 
     @property
     def size(self):
+        """Get the size of the square."""
         return self.__size
 
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
     def area(self):
+        """Calculate the area of the square."""
         return self.__size ** 2
