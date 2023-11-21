9a4898d512fdaddfde06c984f11a485fc60639c2
SphaxWare <itssphax123@gmail.com>
2023-11-21 08:03:50 -0800

    getters and setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            size: square size.
+
+        Raises:
+            TypeError: not int.
+            ValueError: size less than 0.
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if (0 > size):
+            raise ValueError("size must be >= 0")
+
+    def set_size(self, size):
+        self.__size = size
+
+    def get_size(self, size):
+        return self.size
+
+    def area(self):
+        return self.__size ** 2
