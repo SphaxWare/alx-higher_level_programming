8c12269ba026e21d866f170ab7b4c6ff5a596eba
SphaxWare <itssphax123@gmail.com>
2023-11-24 13:26:52 -0800

    task 0 interger addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+"""
+    my integer addition module.
+    >>> add_integer(1, 5)
+    3
+"""
+
+
 def add_integer(a, b=98):
     """Return result of the addition of a and b
-
     >>> add_integer(1, 2)
-    3
-    >>> add_integer(1, 's')
-    Traceback (most recent call last):
-        ...
-    TypeError: b must be an integer
-    """
+    3"""
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
