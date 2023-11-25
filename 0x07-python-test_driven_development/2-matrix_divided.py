6b48205be4618fac8cf3738a32d1c396248d5056
SphaxWare <itssphax123@gmail.com>
2023-11-24 16:13:46 -0800

    fixed pycodestyle

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -17,7 +17,8 @@ def matrix_divided(matrix, div):
     if any(len(row) != row_size for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not all(isinstance(col, (int, float)) for row in matrix for col in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix "
+                        "(list of lists) of integers/floats")
     newmat = [[] for _ in range(len(matrix))]
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
