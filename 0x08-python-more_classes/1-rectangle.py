108f8b159a716df062fe2d789006765d9e0d2fb1
SphaxWare <itssphax123@gmail.com>
2023-11-27 06:21:30 -0800

    added width and height with getters and setters

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Rectangle module.
+"""
+
+
+class Rectangle:
+    """
+    Rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        """
+        Constructor for the Rectangle class.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the Rectangle."""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get height of the Rectangle."""
+        self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the Rectangle."""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
