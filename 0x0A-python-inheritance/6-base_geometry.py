521b212e14aa40ec4a4e7be0b125b4cf85f3d669
SphaxWare <itssphax123@gmail.com>
2023-12-04 07:46:18 -0800

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Geometry module."""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+    def area(self):
+        """raises an Exception"""
+        raise Exception("area() is not implemented")
