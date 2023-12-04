3e5383701800410c49e403d039c1215b3dde5a8a
SphaxWare <itssphax123@gmail.com>
2023-12-04 07:33:49 -0800

    same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""same class"""
+
+
+def is_same_class(obj, a_class):
+    """returns True if the object is exactly
+    an instance of the specified class"""
+    return isinstance(obj, a_class)
