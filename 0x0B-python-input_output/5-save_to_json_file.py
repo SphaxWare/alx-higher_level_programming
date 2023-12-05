d2059d9195eab4698c08f990dc48e7df9ca1b0e1
SphaxWare <itssphax123@gmail.com>
2023-12-05 08:44:04 -0800

    save object to file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Save Object to a file"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """save as json to file"""
+    with open(filename, 'w') as f:
+        json.dump(x, f)
