670f58b010bc30049f4b558e4f280d741faae6bc
SphaxWare <itssphax123@gmail.com>
2023-12-04 16:48:48 -0800

    add attribute

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,7 +2,7 @@
 """att Module"""
 
 
-def add_att(obj, att, value):
+def add_attribute(obj, att, value):
     """Add a new attribute to an object if possible."""
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
