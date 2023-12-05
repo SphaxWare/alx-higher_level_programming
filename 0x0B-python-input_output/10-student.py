d2bf97b8f5aeee2c7672d908256dba2608600c2c
SphaxWare <itssphax123@gmail.com>
2023-12-05 15:26:10 -0800

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Student module"""
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        try:
+            for i in attrs:
+                if type(i) is not str:
+                    return self.__dict__
+        except Exception:
+            return self__dict__
+        obj = dict()
+        for k, v in self.__dict__.itemps():
+            if k in attrs:
+                obj[k] = v
+        return obj
