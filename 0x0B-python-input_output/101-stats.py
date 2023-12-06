8d973c3193e8be01163b6f1ad853ee88b398401e
SphaxWare <itssphax123@gmail.com>
2023-12-05 18:59:19 -0800

    test

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,48 +1,41 @@
 #!/usr/bin/python3
 """stats module"""
-import sys
+from sys import stdin
 
 
-def print_statistics(total_size, status_counts):
-    """
-    Print the computed statistics.
-    """
-    print("File size: {}".format(total_size))
-    for code in sorted(status_counts.keys()):
-        print("{}: {}".format(code, status_counts[code]))
+total_size = i = 0
+status_counts = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+        }
 
 
-def main():
-    total_size = 0
-    status_counts = {
-        200: 0,
-        301: 0,
-        400: 0,
-        401: 0,
-        403: 0,
-        404: 0,
-        405: 0,
-        500: 0
-    }
-
-    try:
-        for i, line in enumerate(sys.stdin, 1):
-            tokens = line.split()
-            if len(tokens) >= 9:
-                status_code = int(tokens[-2])
-                file_size = int(tokens[-1])
-                total_size += file_size
-                if status_code in status_counts:
-                    status_counts[status_code] += 1
-
-            if i % 10 == 0:
-                print_statistics(total_size, status_counts)
-
-    except KeyboardInterrupt:
-        # Handle KeyboardInterrupt (CTRL+C)
-        print_statistics(total_size, status_counts)
-        sys.exit(0)
+def showstats():
+    """this func print stats"""
+    print(f'File size: {total_size}')
+    for k, v in sorted(status_counts.items()):
+        if v > 0:
+            print('{:s}: {:d}'.format(k, v))
 
 
-if __name__ == "__main__":
-    main()
+try:
+    for l in stdin:
+        splitline = l.split()
+        if len(splitline) >= 2:
+            status = splitline[-2]
+            total_size += int(splitline[-1])
+            if status in status_counts:
+                status_counts[status] += 1
+        i += 1
+
+        if i % 10 == 0:
+            showstats()
+    showstats()
+except KeyboardInterrupt as e:
+    showstats()
