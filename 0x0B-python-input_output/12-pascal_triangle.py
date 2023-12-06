3513631f67abceabb3e53c749068535f43989db9
SphaxWare <itssphax123@gmail.com>
2023-12-05 17:35:11 -0800

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Triangle Module."""
+
+
+def pascal_triangle(n):
+    """pascal triangle"""
+    if n <= 0:
+        return l
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
