1f1c5c8254d80c5659e126e4b9c7498a5c02a5ca
Sphax <itssphax123@gmail.com>
2023-12-09 15:45:04 -0800

    quick fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
         return (
                 f"[Square] ({self.id}) "
                 f"{self.x}/{self.y} - "
-                f"{self.width}/{self.height}"
+                f"{self.size}"
                 )
 
     @property
@@ -23,3 +23,16 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update the attributes"""
+        if args:
+            self.id = args[0] if len(args) >= 1 else self.id
+            self.size = args[1] if len(args) >= 2 else self.size
+            self.x = args[2] if len(args) >= 3 else self.x
+            self.y = args[3] if len(args) >= 4 else self.y
+        elif kwargs:
+            self.id = kwargs.get('id', self.id)
+            self.size = kwargs.get('size', self.size)
+            self.x = kwargs.get('x', self.x)
+            self.y = kwargs.get('y', self.y)
