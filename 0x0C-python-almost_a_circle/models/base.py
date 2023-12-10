8405f3c8da573233ee2d06134447b0b677135803
Sphax <itssphax123@gmail.com>
2023-12-09 16:12:46 -0800

    15. Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """base module"""
+import json
 
 
 class Base:
@@ -12,3 +13,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries:
+            return json.dumps(list_dictionaries)
+        else:
+            return "[]"
