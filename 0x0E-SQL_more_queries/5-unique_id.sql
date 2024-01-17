466bca03255f0e19c6ef2716165b85aeeb9edf45
Sphax <itssphax123@gmail.com>
2024-01-17 14:25:28 -0800

    5. Unique ID

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- creates the table unique_id
+-- cat 5-unique_id.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
+CREATE TABLE IF NOT EXISTS unique_id (
+	id INT DEFAULT 1 UNIQUE,
+	name VARCHAR(256)
+);
