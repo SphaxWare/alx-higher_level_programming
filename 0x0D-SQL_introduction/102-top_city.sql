3f0038f2e1c3fa0fd4dc1b81694c793aad882234
Sphax <itssphax123@gmail.com>
2024-01-16 16:07:40 -0800

    19. Temperatures #1

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,5 @@
+-- displays the top 3 of cities temperature during July and August
+-- ordered by temperature (descending)
+SELECT city, AVG(value) as avg_temp FROM temperatures
+WHERE month = 'July' or month='August'
+GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
