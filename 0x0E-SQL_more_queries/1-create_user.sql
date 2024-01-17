404f19266675736e18eee69f043d9fcc9a95f740
Sphax <itssphax123@gmail.com>
2024-01-17 14:00:18 -0800

    1. Root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- script that creates the MySQL server user user_0d_1
+-- cat 1-create_user.sql | mysql -hlocalhost -uroot -p
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
