f32cbc5cdacc5870efa99503e041c08573263e12
Sphax <itssphax123@gmail.com>
2024-01-17 17:33:11 -0800

    14. My genres

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- all genres contained in hbtn_0d_ and the number of shows
+SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
