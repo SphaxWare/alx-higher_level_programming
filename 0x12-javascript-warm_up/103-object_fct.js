db7cf83579c49041d53d0fd2d94369b2227b0313
SphaxWare <itssphax123@gmail.com>
2024-02-13 20:40:53 +0000

    advanced

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12,
+  incr: function () {
+    this.value++;
+  }
+};
+
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
