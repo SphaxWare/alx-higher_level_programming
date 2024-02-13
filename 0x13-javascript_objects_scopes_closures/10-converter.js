feeba224784374c1c4dc8c0f82c96d856190dba9
SphaxWare <itssphax123@gmail.com>
2024-02-13 16:15:46 +0000

    fix

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,17 +1,4 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  return function (number) {
-    if (number === 0) {
-      return 0;
-    } else {
-      const remainder = number % base;
-      const quotient = Math.floor(number / base);
-
-      if (quotient === 0) {
-        return remainder.toString(base);
-      } else {
-        return exports.converter(base)(quotient) + remainder.toString(base);
-      }
-    }
-  };
+  return num => num.toString(base);
 };
