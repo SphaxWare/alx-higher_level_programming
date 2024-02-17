0ea59de27f559243d8fdad0c8ab15935fe2a5fc7
Sphax <itssphax123@gmail.com>
2024-02-17 02:58:09 -0800

    8. First state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -19,5 +19,8 @@ if __name__ == "__main__":
     session = Session()
     Base.metadata.create_all(engine)
     state = session.query(State).order_by(State.id).first()
-    print("{}: {}".format(state.id, state.name))
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
     session.close()
