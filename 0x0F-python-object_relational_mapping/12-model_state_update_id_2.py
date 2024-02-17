c6faa02bcc9288785bf4ca2954c992bc392a454a
Sphax <itssphax123@gmail.com>
2024-02-17 04:50:30 -0800

    update a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects
+"""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    # creating an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # Creating a Session class using sessionmaker
+    Session = sessionmaker(bind=engine)
+    # Creating an instance of the Session class
+    session = Session()
+    Base.metadata.create_all(engine)
+    # Create a new State object
+    state = session.query(State).filter_by(id=2).first()
+    if state:
+        state.name = 'New Mexico'
+        session.commit()
+    session.close()
