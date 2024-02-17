86defd25c9cd63637dd5c08051caa9d14cd62047
Sphax <itssphax123@gmail.com>
2024-02-17 04:56:25 -0800

    delete state by name

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects
+"""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    # creating an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # Creating a Session class using sessionmaker
+    Session = sessionmaker(bind=engine)
+    # Creating an instance of the Session class
+    session = Session()
+    states = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states:
+        state.delete(state)
+    session.commit()
+    session.close()
