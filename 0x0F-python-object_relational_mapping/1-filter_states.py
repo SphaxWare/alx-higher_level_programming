bc77bdad90c445cb5af6abd1f191a385efe7fee4
Sphax <itssphax123@gmail.com>
2024-02-16 21:28:49 -0800

    1. Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Script that lists all states"""
+import MySQLdb
+import sys
+
+# The code should not be executed when imported
+if __name__ == '__main__':
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+    # Connect to db
+    db = MySQLdb.connect(host='localhost',
+                         user=MY_USER,
+                         passwd=MY_PASS,
+                         db=MY_DB)
+    cur = db.cursor()
+    cur.execute("SELECT * from states WHERE name REGEXP '^[N]';")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    # Closing db
+    db.close()
