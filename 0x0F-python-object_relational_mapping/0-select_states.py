c687a9d527765b0b84460678bd4c344ee93ca047
Sphax <itssphax123@gmail.com>
2024-02-16 21:20:46 -0800

    close db

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
+""" Script that lists all states"""
 import MySQLdb
 import sys
-"""
- lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
-"""
-def main():
+
+
+if __name__ == '__main__':
     MY_USER = sys.argv[1]
     MY_PASS = sys.argv[2]
     MY_DB = sys.argv[3]
-    db = MySQLdb.connect(host='localhost', user=MY_USER, passwd=MY_PASS, db=MY_DB)
+    db = MySQLdb.connect(host='localhost',
+                         user=MY_USER,
+                         passwd=MY_PASS,
+                         db=MY_DB)
     cur = db.cursor()
     cur.execute("SELECT * from states ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
-
-if __name__ == '__main__':
-    main()
+    db.close()
