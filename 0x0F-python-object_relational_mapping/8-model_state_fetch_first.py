5d23d733997ef5d0f2fd14e48918fe82361aec3b
Sphax <itssphax123@gmail.com>
2024-02-17 02:46:07 -0800

    first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects
+"""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    # creating an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # Creating a Session class using sessionmaker
+    Session = sessionmaker(bind=engine)
+    # Creating an instance of the Session class
+    session = Session()
+    Base.metadata.create_all(engine)
+    state = session.query(State).order_by(State.id).first()
+    print("{}: {}".format(state.id, state.name))
+    session.close()
