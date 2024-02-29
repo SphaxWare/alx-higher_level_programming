5c2fbde2fa257c44ed96a89ebce2ff3835ef167b
Sphax <itssphax123@gmail.com>
2024-02-29 05:25:27 -0800

    fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # sends a GET request to the URL, and displays the body of the response
-curl -sI "$1" | grep "HTTP/1.1 200 OK" > /dev/null
-if [ "$?" -eq 0 ]; then
-	curl -s "$1"
+if curl -sI "$1" | grep -q "HTTP/1.1 200 OK"; then
+    curl -s "$1"
 fi
