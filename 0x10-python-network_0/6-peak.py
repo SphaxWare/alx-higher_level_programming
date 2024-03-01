a640a0880678324298bd1dd05c37f198b2fd35d6
SphaxWare <134861101+SphaxWare@users.noreply.github.com>
2024-03-01 01:59:20 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 """Peak from a list of unsorted ints"""
+
+
 def find_peak(list_of_integers):
+    """
+    find the peak unt of a list
+    """
     newlist = sorted(list_of_integers)
     return (newlist[-1])
