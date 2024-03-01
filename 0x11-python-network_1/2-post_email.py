8deef0099b575eb721cf3cf77ddd9ecd6032eec3
Sphax <itssphax123@gmail.com>
2024-03-01 11:53:07 -0800

    fix pycodestule

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode({'email':email}).encode('utf-8')
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         content = response.read().decode('utf-8')
