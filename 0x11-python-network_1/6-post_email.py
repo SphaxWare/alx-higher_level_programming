f9e80fa8d10c6672a2d13e839b928f8334e5f25c
Sphax <itssphax123@gmail.com>
2024-03-01 15:59:41 -0800

    fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -16,5 +16,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = {'email': email}
-    response = requests.post(url, data=data)
-    print("Your email is: {}".format(response.text))
+    response = requests.post(url, data)
+    print(response.text)
