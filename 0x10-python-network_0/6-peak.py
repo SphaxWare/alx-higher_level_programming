ca9af0eefe95c8af1a4359f78d04e82220a8fdfb
Sphax <itssphax123@gmail.com>
2024-02-29 17:31:14 -0800

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,5 +6,17 @@ def find_peak(list_of_integers):
     """
     find the peak unt of a list
     """
-    newlist = sorted(list_of_integers)
-    return (newlist[-1])
+    nums = list_of_integers
+    l, r = 0, len(list_of_integers) - 1
+
+
+    while l <= r:
+        m = l + ((r - l) // 2)
+        # left greater
+        if m > 0 and nums[m] < nums[m - 1]:
+            r = m - 1
+        # right is grater
+        elif m < len(nums) - 1 and nums[m] < nums[m + 1]:
+            l = m + 1
+        else:
+            return m
