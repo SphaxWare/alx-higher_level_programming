66903242ca43d67ef300b7c881b602d4604e92ce
Sphax <itssphax123@gmail.com>
2024-03-01 12:33:38 -0800

    with requests package

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
+import requests
 
 if __name__ == "__main__":
     """Main module"""
     url = "https://alx-intranet.hbtn.io/status"
-
-    with urllib.request.urlopen(url) as response:
-        content_type = response.getheader('Content-Type')
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content_type)))
-        print("\t- content: {}".format(content))
+    response = requests.get(url)
+    content = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
