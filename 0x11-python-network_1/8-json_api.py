b797b6bb16fd474cf97a5231e6adc1a930d9da57
Sphax <itssphax123@gmail.com>
2024-03-01 16:20:41 -0800

    fix ?

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     # Set q to the given letter or an empty string if no argument is given
     q = sys.argv[1] if len(sys.argv) > 1 else ""
-    data = {'q': q}
+    letter = {'q': q}
     try:
-        response = requests.post(url, data=data)
+        response = requests.post(url, data=letter)
         json_data = response.json()
         if json_data:
             print("[{}] {}".format(json_data.get('id'), json_data.get('name')))
