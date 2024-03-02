15270bc901e0f1abcfb83075e7746243c618efbe
Sphax <itssphax123@gmail.com>
2024-03-01 16:09:07 -0800

    fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,9 +5,8 @@ import requests
 if __name__ == "__main__":
     """Main module"""
     url = "https://alx-intranet.hbtn.io/status"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
         print(response.text)
-    except requests.exceptions.HTTPError as e:
-        print("Error code: {}".format(e.response.status_code))
