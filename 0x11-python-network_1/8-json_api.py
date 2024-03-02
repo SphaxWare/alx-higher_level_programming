229b501322ba2f61bbd940eaf11221699d12a946
Sphax <itssphax123@gmail.com>
2024-03-01 16:17:03 -0800

    8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    
+    # Set q to the given letter or an empty string if no argument is given
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    data = {'q': q}
+
+    try:
+        response = requests.post(url, data=data)
+        json_data = response.json()
+
+        if json_data:
+            print("[{}] {}".format(json_data.get('id'), json_data.get('name')))
+        else:
+            print("No result")
+
+    except ValueError:
+        print("Not a valid JSON")
+
