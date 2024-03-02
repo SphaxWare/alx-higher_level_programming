5d0dfeb882801c4b970df17d3fff9dc74c552835
Sphax <itssphax123@gmail.com>
2024-03-01 16:18:42 -0800

    fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,20 +8,15 @@ import sys
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    
     # Set q to the given letter or an empty string if no argument is given
     q = sys.argv[1] if len(sys.argv) > 1 else ""
     data = {'q': q}
-
     try:
         response = requests.post(url, data=data)
         json_data = response.json()
-
         if json_data:
             print("[{}] {}".format(json_data.get('id'), json_data.get('name')))
         else:
             print("No result")
-
     except ValueError:
         print("Not a valid JSON")
-
