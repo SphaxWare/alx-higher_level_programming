29b5a271b3becc13b7296c285dd66172579df2a0
Sphax <itssphax123@gmail.com>
2024-03-26 11:45:31 -0700

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,6 @@ const apiUrl = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/people/18/';
 request.get(apiUrl, (error, response, body) => {
   if (error) {
-    console.error(error);
     return;
   }
   const films = JSON.parse(body).results;
