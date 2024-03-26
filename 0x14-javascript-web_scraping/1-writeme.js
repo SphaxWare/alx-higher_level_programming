26d3dc64a9cbb5b0416610bc80bc99bbdb96b414
Sphax <itssphax123@gmail.com>
2024-03-26 10:29:44 -0700

    write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filename = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(filename, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
