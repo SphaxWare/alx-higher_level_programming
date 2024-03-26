eb0596e33be3c030d1215afa129a9784798fe83d
Sphax <itssphax123@gmail.com>
2024-03-26 11:39:06 -0700

    4. Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/people/18/';
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const films = JSON.parse(body).results;
+  let count = 0;
+  films.forEach(film => {
+    if (film.characters.includes(url)) {
+      count++;
+    }
+  });
+  console.log(count);
+});
